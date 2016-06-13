672408f7779b43ce119c3ed5e93c07d62497d9c0
chadmadna <chadmadna@gmail.com>
2016-06-13 20:33:20 +0700

    WIP: Implemented ruleset, implementing accuracy

--- id3.py
+++ id3.py
@@ -13,15 +13,17 @@ class Dataset(object):
         self.data = []
         self.headers = None
         self.target = None
+        self.tree = None
+        self.ruleset = None
         self._is_formatted = is_formatted
         self.populate_data(data_list)
-        print("Initiated dataset...\nHeaders: {}\nTarget: {}".format
+        print("Initiating dataset...\nHeaders: {}\nTarget: {}".format
               (", ".join(self.headers), self.target))
 
     def populate_data(self, data_list):
         """Populates the dataset with the given pre-processed list of
         lines read from the .txt file. Each tuple is stored as a
-        namedtuple."""
+        dictionary."""
         headers = data_list[0].rstrip().split('\t')
         if self._is_formatted:
             self.headers = headers[:-1]
@@ -61,35 +63,73 @@ class Dataset(object):
         splitting target attribute r with splitting attribute s."""
         return self.entropy(r) - self.entropy(r, s)
 
-    def id3(self, target, attrs, dataset):
+    def _id3(self, target, attrs, dataset):
         """Recursively constructs a decision tree given a target
         attribute, a list of predicting attributes, and an example
         dataset. Returns a tree of Node objects."""
         t_values = [tup[target] for tup in dataset]
         if len(set(t_values)) == 1:
             val = set(t_values).pop()
-            return Node("{}: {}".format(target, val))
+            return Node({'attr': target, 'val': val}, 'label')
         elif not attrs:
             common = max(set(t_values), key=t_values.count)
-            return Node("{}: {}".format(target, common))
+            return Node({'attr': target, 'val': common}, 'label')
         else:
             info_gain = {a: self.information_gain(target, a) for a in attrs}
             attr = max(attrs, key=lambda x: info_gain[x])
-            root = Node(attr)
+            root = Node(attr, 'attr')
             a_values = sorted(set([tup[attr] for tup in dataset]))
             for val in a_values:
-                child = root.add(Node(val))
+                child = root.add(Node(val, 'val'))
                 subset = [tup for tup in dataset if tup[attr] == val]
                 if not subset:
                     subset_t_values = [tup[target] for tup in subset]
                     common = max(set(subset_t_values), key=subset_t_values.count)
-                    child.add(Node("{}: {}".format(target, common)))
+                    child.add(Node({'attr': target, 'val': common}, 'label'))
                 else:
                     if attrs and attr in attrs:
                         attrs.remove(attr)
-                    child.add(self.id3(target, attrs, subset))
+                    child.add(self._id3(target, attrs, subset))
 
             return root
+
+    def build_tree(self):
+        self.tree = self._id3(self.target, self.headers, self.data)
+        return self.tree
+
+    def derive_ruleset(self):
+        if self.tree is None:
+            self.build_tree()
+        self.ruleset = [rule for rule in self.paths(self.tree)]
+
+    def print_ruleset(self):
+        if self.ruleset is None:
+            self.derive_ruleset()
+        print("\nRuleset: ")
+        for rule in self.ruleset:
+            pred, result = rule[:-1], rule[-1]
+            attr_pairs = tuple(zip(*(iter(pred),) * 2))
+            rulestr = 'IF '
+            for i in attr_pairs:
+                rulestr += "({} == {}) AND ".format(i[0], i[1])
+            rulestr = rulestr[:-4] + "THEN " + "{} = {}".format(
+                result['attr'], result['val'])
+            print(rulestr)
+
+    def accuracy(self):
+        if self.tree is None:
+            raise ValueError("tree is empty")
+        for rule in self.ruleset:
+            pass
+
+    def paths(self, node, path=None):
+        if path is None:
+            path = []
+        if node.is_leaf:
+            yield path + [node.value]
+        for child in node.children:
+            for leaf_path in self.paths(child, path + [node.value]):
+                yield leaf_path
 
 
 class Node(object):
@@ -97,19 +137,31 @@ class Node(object):
     of representing a decision tree with arbitrary labels
     and number of children."""
 
-    def __init__(self, value, children=None):
+    def __init__(self, value, type, children=None):
         self.value = value
+        self.type = type
         self.children = [] if children is None else children
 
-    def __repr__(self):
-        return str(self.value)
+    def __str__(self):
+        if self.type == 'attr':
+            return str(self.value)
+        elif self.type == 'val':
+            return str(self.value)
+        elif self.type == 'label':
+            attr = str(self.value['attr'])
+            val = str(self.value['val'])
+            return "{}: {}".format(attr, val)
+
+    @property
+    def is_leaf(self):
+        return self.type == 'label'
 
     def add(self, child):
         self.children.append(child)
         return child
 
     def print(self, lvl=0, tab_lvl=0):
-        print('\t'*lvl + '  \_'*tab_lvl + self.value)
+        print('\t'*lvl + '  \_'*tab_lvl + str(self))
         if tab_lvl:
             lvl += 1
         if not tab_lvl:
@@ -122,6 +174,5 @@ if __name__ == '__main__':
     with open('data_titanic.txt') as fd:
         f = fd.readlines()
     d = Dataset(f)
-    t = d.id3(d.target, d.headers, d.data)
-    t.print()
-
+    d.build_tree().print()
+    d.print_ruleset()
